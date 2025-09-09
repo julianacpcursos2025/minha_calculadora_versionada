e9cd71ce7bd7a485bd9e2e1dd72c8f1cc87ceb99
julianacpcursos2025 <julianacpcursos2025@gmail.com>
2025-09-09 15:02:10 -0300

    Etapa 1: Adiciona a função de soma

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+#calculadora
+
+num1= int(input("Insira primeiro número: "))
+num2= int(input("Insira segundo número: "))
+opcao= input("Escolha uma opção (+, -, *, /): ")
+
+def somar (num1, num2):
+    soma= num1 + num2
+    return soma
+
